8069ac31a234da5729c59c342805e64a5eb1523c
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-10-18 09:48:27 -0400

    created it so the user could input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,11 +1,14 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner keyboard;
+        keyboard = new Scanner (System.in);
         System.out.println("Hello World");
 
 
-        
     }
 }
