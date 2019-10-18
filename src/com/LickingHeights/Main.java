fea1e10572b03dbecd4f29e2591ad6653f101acf
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-10-18 10:01:01 -0400

    made it so that whatever name they input shows up

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner keyboard;
-        keyboard = new Scanner (System.in);
+
         System.out.println("Hello World");
 
 
     }
+}
+    public static void printName (String name ){
+    Scanner keyboard;
+    keyboard = new Scanner (System.in);
+
+    System.out.println("Hello "+ name );
+    name = keyboard.nextLine();
+
 }
